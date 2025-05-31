b95ed3d77d0ce0461c108ae54031e2ae1fe9087f
ChesterCaii <chestercai5@gmail.com>
2025-05-31 13:14:59 -0700

    Add asteroid and asteroid field with spawn behavior

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
-from constants import * 
-import pygame
-def main():
-	print("Starting Asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
-	pygame.init()
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	while True:
-		for event in pygame.event.get():
-   			if event.type == pygame.QUIT:
-       				return	
-	screen.fill((0,0,0))
-	pygame.display.flip()
+from constants import *  
+from player import Player 
+import pygame 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
-if __name__ == "__main__":
+
+def main(): 
+	print("Starting Asteroids!") 
+	print(f"Screen width: {SCREEN_WIDTH}") 
+	print(f"Screen height: {SCREEN_HEIGHT}") 
+
+	pygame.init() 
+	clock = pygame.time.Clock() 
+	dt = 0 
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+	
+	updateable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()	
+	asteroids = pygame.sprite.Group()	
+	Player.containers = (updateable, drawable)
+	Asteroid.containers = (asteroids, updateable, drawable)
+	AsteroidField.containers = (updateable)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroid_field = AsteroidField()
+	while True: 
+		for event in pygame.event.get(): 
+			if event.type == pygame.QUIT: 
+				return 
+
+		updateable.update(dt)
+		screen.fill((0, 0, 0)) 
+		for sprite in drawable:
+			sprite.draw(screen) 
+		pygame.display.flip() 
+		dt = clock.tick(60) / 1000 
+
+
+
+
+
+if __name__ == "__main__": 
     main()
+
