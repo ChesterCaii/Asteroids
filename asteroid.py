43f2e6efa78f210ec86388d73bba86ebf067d89d
ChesterCaii <chestercai5@gmail.com>
2025-05-31 13:53:16 -0700

    Implement asteroid splitting into smaller asteroids on hit

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,8 @@
 import pygame
 from circleshape import CircleShape
+import random
+from constants import ASTEROID_MIN_RADIUS
+
 
 class Asteroid(CircleShape):
 	def __init__(self, x, y, radius):
@@ -11,4 +14,28 @@ class Asteroid(CircleShape):
 	def update(self, dt):
    		self.position += self.velocity * dt
 		
-	
+	def split(self):
+    		# Always destroy the current asteroid
+		self.kill()
+
+		# If it's already the smallest size, just die
+		if self.radius <= ASTEROID_MIN_RADIUS:
+			return
+
+		# Calculate new smaller radius
+		new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+		# Create two new velocities at slight angles
+		angle = random.uniform(20, 50)
+		v1 = self.velocity.rotate(angle) * 1.2
+		v2 = self.velocity.rotate(-angle) * 1.2
+
+		# Spawn two new asteroids at the same position
+		from asteroid import Asteroid  # avoid circular import issues
+
+		a1 = Asteroid(self.position.x, self.position.y, new_radius)
+		a1.velocity = v1
+
+		a2 = Asteroid(self.position.x, self.position.y, new_radius)
+		a2.velocity = v2
+
